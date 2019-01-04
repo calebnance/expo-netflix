55dec65e98d1d3de6d4dfd397997adeb4daac353
Caleb Nance <calebnance@gmail.com>
2019-01-04 02:54:34 -0500

    bandersnatch updated

--- src/api/preloadImages.js
+++ src/api/preloadImages.js
@@ -1,7 +1,7 @@
 const netflix = require('../assets/images/netflix-logo.png');
 const netflixFull = require('../assets/images/netflix-logo-full.png');
 
-const bannerBander = require('../assets/images/content/black-mirror-bandersnatch.png');
+const bannerBander = require('../assets/images/content/black-mirror-bandersnatch.jpg');
 
 export default {
   netflix,
