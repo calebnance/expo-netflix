1739f88150928e40a872c71845c821512e03883b
Caleb Nance <calebnance@gmail.com>
2019-01-04 01:26:51 -0500

    Downloads screen is at a good state

--- src/components/icons/Svg.Downloads.js
+++ src/components/icons/Svg.Downloads.js
@@ -4,17 +4,23 @@ import PropTypes from 'prop-types';
 import { colors } from '../../api/constants';
 
 const SvgDownloads = props => {
-  const { active, size } = props;
+  const { active, fill, size } = props;
+
+  let fillColor = fill;
+
+  if (fillColor === null) {
+    fillColor = active ? colors.white : colors.inactiveGrey;
+  }
 
   return (
     <Svg height={size} width={size} viewBox="0 0 24 24">
       <Svg.Path
         d="M21 14c-.6 0-1 .4-1 1v4c0 .6-.4 1-1 1H5c-.6 0-1-.4-1-1v-4c0-.6-.4-1-1-1s-1 .4-1 1v4c0 1.7 1.3 3 3 3h14c1.7 0 3-1.3 3-3v-4c0-.6-.4-1-1-1z"
-        fill={active ? colors.white : colors.inactiveGrey}
+        fill={fillColor}
       />
       <Svg.Path
         d="M11.3 15.7c.1.1.2.2.3.2.1.1.3.1.4.1s.3 0 .4-.1c.1-.1.2-.1.3-.2l5-5c.4-.4.4-1 0-1.4s-1-.4-1.4 0L13 12.6V3c0-.6-.4-1-1-1s-1 .4-1 1v9.6L7.7 9.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l5 5z"
-        fill={active ? colors.white : colors.inactiveGrey}
+        fill={fillColor}
       />
     </Svg>
   );
@@ -22,12 +28,14 @@ const SvgDownloads = props => {
 
 SvgDownloads.defaultProps = {
   active: true,
+  fill: null,
   size: 24
 };
 
 SvgDownloads.propTypes = {
   // optional
   active: PropTypes.bool,
+  fill: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   size: PropTypes.number
 };
 
--- src/screens/Downloads.js
+++ src/screens/Downloads.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { colors, gStyle } from '../api/constants';
 
 import Cast from '../components/Cast';
 import Header from '../components/Header';
+
+import SvgDownloads from '../components/icons/Svg.Downloads';
 
 const Downloads = props => {
   const { navigation } = props;
@@ -18,6 +20,18 @@ const Downloads = props => {
         title="My Downloads"
       />
 
+      <View style={styles.containerIcon}>
+        <SvgDownloads fill={colors.bgGrey} size={120} />
+      </View>
+
+      <Text style={styles.description}>
+        Movies and TV shows that you download appear here.
+      </Text>
+
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>FIND SOMETHING TO DOWNLOAD</Text>
+      </View>
+
       <Cast navigation={navigation} />
     </View>
   );
@@ -27,5 +41,41 @@ Downloads.propTypes = {
   // required
   navigation: PropTypes.object.isRequired
 };
+
+const styles = StyleSheet.create({
+  containerIcon: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.downloadsIconBg,
+    borderRadius: 96,
+    height: 192,
+    justifyContent: 'center',
+    marginBottom: 32,
+    marginTop: 48,
+    width: 192
+  },
+  description: {
+    alignSelf: 'center',
+    color: colors.white,
+    fontFamily: 'HelveticaNeue',
+    fontSize: 16,
+    marginBottom: 48,
+    textAlign: 'center',
+    width: 300
+  },
+  button: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderColor: colors.white,
+    borderWidth: 1,
+    justifyContent: 'center',
+    padding: 16
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    textAlign: 'center'
+  }
+});
 
 export default Downloads;
