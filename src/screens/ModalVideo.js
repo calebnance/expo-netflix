716eef8990b3269fd456cc5752f100c0d5ba7004
Caleb Nance <calebnance@gmail.com>
2019-01-15 00:05:01 -0500

    Modal Video player screen added

--- src/screens/ModalVideo.js
+++ src/screens/ModalVideo.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ScreenOrientation } from 'expo';
 import { gStyle } from '../api/constants';
 
-const ModalVideo = () => (
-  <View style={gStyle.container}>
-    <Text style={gStyle.heading}>Modal :: Video</Text>
-  </View>
-);
+class ModalVideo extends React.Component {
+  componentDidMount() {
+    ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
+  }
+
+  componentWillUnmount() {
+    ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT_UP);
+  }
+
+  render() {
+    return (
+      <View style={gStyle.container}>
+        <Text style={gStyle.heading}>Modal :: Video</Text>
+      </View>
+    );
+  }
+}
 
 export default ModalVideo;
