9add4a71b937c8391a39158e019f531d17694f91
Caleb Nance <calebnance@gmail.com>
2019-01-24 01:41:44 -0500

    start of manage profiles modal styling

--- src/screens/ModalManageProfiles.js
+++ src/screens/ModalManageProfiles.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { gStyle } from '../api/constants';
+import { colors, gStyle, images } from '../api/constants';
 
 import Header from '../components/Header';
 
@@ -9,8 +9,19 @@ const ModalManageProfiles = props => {
   const { navigation } = props;
 
   return (
-    <View style={gStyle.container}>
+    <View style={[gStyle.container, { backgroundColor: colors.black }]}>
       <Header close closeText="Close" navigation={navigation} showLogo />
+
+      <View style={styles.container}>
+        <View style={styles.containerUser}>
+          <Image source={images.robot} style={styles.avatar} />
+          <Text style={[styles.username, styles.usernameActive]}>Caleb</Text>
+        </View>
+        <View style={styles.containerUser}>
+          <Image source={images.penguin} style={styles.avatar} />
+          <Text style={styles.username}>Kim</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -19,5 +30,21 @@ ModalManageProfiles.propTypes = {
   // required
   navigation: PropTypes.object.isRequired
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    paddingVertical: 60,
+    width: 264
+  },
+  avatar: {
+    height: 94,
+    resizeMode: 'contain',
+    width: 94
+  }
+});
 
 export default ModalManageProfiles;
