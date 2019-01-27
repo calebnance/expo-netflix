4bf5d942d08cc47d32b71bdeeec9553a3be44900
Caleb Nance <calebnance@gmail.com>
2019-01-26 23:52:45 -0500

    manage profiles a little closer

--- src/api/colors.js
+++ src/api/colors.js
@@ -1,6 +1,8 @@
 export default {
   black: '#000',
   black20: 'rgba(0, 0, 0, 0.2)',
+  black40: 'rgba(0, 0, 0, 0.4)',
+  black50: 'rgba(0, 0, 0, 0.5)',
   white: '#fff',
 
   // shared
--- src/screens/ModalManageProfiles.js
+++ src/screens/ModalManageProfiles.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { colors, gStyle, images } from '../api/constants';
+import { colors, fonts, gStyle, images } from '../api/constants';
 
 import Header from '../components/Header';
+import SvgPlus from '../components/icons/Svg.Plus';
 
 const ModalManageProfiles = props => {
   const { navigation } = props;
@@ -14,12 +15,20 @@ const ModalManageProfiles = props => {
 
       <View style={styles.container}>
         <View style={styles.containerUser}>
+          <View style={styles.overlay} />
           <Image source={images.robot} style={styles.avatar} />
-          <Text style={[styles.username, styles.usernameActive]}>Caleb</Text>
+          <Text style={styles.text}>Caleb</Text>
         </View>
         <View style={styles.containerUser}>
+          <View style={styles.overlay} />
           <Image source={images.penguin} style={styles.avatar} />
-          <Text style={styles.username}>Kim</Text>
+          <Text style={styles.text}>Kim</Text>
+        </View>
+        <View style={styles.containerUser}>
+          <View style={styles.containerPlus}>
+            <SvgPlus active={false} size={40} />
+          </View>
+          <Text style={styles.text}>Add Profile</Text>
         </View>
       </View>
     </View>
@@ -35,15 +44,43 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: 'center',
     flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'space-between',
     paddingHorizontal: 16,
     paddingVertical: 60,
-    width: 264
+    width: 280
+  },
+  containerUser: {
+    marginBottom: 16
+  },
+  overlay: {
+    backgroundColor: colors.black50,
+    height: 108,
+    top: 0,
+    position: 'absolute',
+    width: 108,
+    zIndex: 10
   },
   avatar: {
-    height: 94,
+    height: 108,
     resizeMode: 'contain',
-    width: 94
+    width: 108
+  },
+  text: {
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: fonts.regular,
+    marginTop: 8,
+    textAlign: 'center'
+  },
+  containerPlus: {
+    alignItems: 'center',
+    backgroundColor: colors.moreAddProfileBg,
+    borderRadius: 54,
+    height: 108,
+    justifyContent: 'center',
+    marginBottom: 4,
+    width: 108
   }
 });
 
