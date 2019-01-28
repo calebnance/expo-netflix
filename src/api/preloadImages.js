0bac86d1886fe5635edd056261648220e0499d60
Caleb Nance <calebnance@gmail.com>
2019-01-28 01:50:40 -0500

    actual show/movie images added

--- src/api/preloadImages.js
+++ src/api/preloadImages.js
@@ -5,6 +5,10 @@ const netflixFull = require('../assets/images/netflix-logo-full.png');
 const bannerBander = require('../assets/images/content/black-mirror-bandersnatch.jpg');
 const logoBander = require('../assets/images/content/bandersnatch-logo.jpg');
 
+const peakyBlinders = require('../assets/images/content/peaky-blinders.jpg');
+const goodWillHunting = require('../assets/images/content/good-will-hunting.jpg');
+const johnMulaneyKG = require('../assets/images/content/john-mulaney-kid-gorgeous.jpg');
+
 const mask = require('../assets/images/user-icons/mask.jpg');
 const penguin = require('../assets/images/user-icons/purple-penguin.jpg');
 const robot = require('../assets/images/user-icons/robot.jpg');
@@ -16,6 +20,10 @@ export default {
 
   bannerBander,
   logoBander,
+
+  peakyBlinders,
+  goodWillHunting,
+  johnMulaneyKG,
 
   mask,
   penguin,
