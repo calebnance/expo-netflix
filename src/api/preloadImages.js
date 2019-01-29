75ee45378e8665199b43002e783f9dbb16874fa5
Caleb Nance <calebnance@gmail.com>
2019-01-28 20:54:39 -0500

    more assets added

--- src/api/preloadImages.js
+++ src/api/preloadImages.js
@@ -5,6 +5,15 @@ const netflixFull = require('../assets/images/netflix-logo-full.png');
 const bannerBander = require('../assets/images/content/black-mirror-bandersnatch.jpg');
 const logoBander = require('../assets/images/content/bandersnatch-logo.jpg');
 
+// circle
+const americanGangster = require('../assets/images/content/circle_american-gangster.png');
+const breakingBad = require('../assets/images/content/circle_breaking-bad.png');
+const starwarsJedi = require('../assets/images/content/circle_star-wars-the-last-jedi.png');
+const guardians2 = require('../assets/images/content/circle_guardians-vol2.png');
+const darkKnight = require('../assets/images/content/circle_the-dark-knight.png');
+const blackPanther = require('../assets/images/content/circle_black-panther.png');
+
+// rectangle
 const peakyBlinders = require('../assets/images/content/peaky-blinders.jpg');
 const goodWillHunting = require('../assets/images/content/good-will-hunting.jpg');
 const johnMulaneyKG = require('../assets/images/content/john-mulaney-kid-gorgeous.jpg');
@@ -20,6 +29,13 @@ export default {
 
   bannerBander,
   logoBander,
+
+  americanGangster,
+  breakingBad,
+  starwarsJedi,
+  guardians2,
+  darkKnight,
+  blackPanther,
 
   peakyBlinders,
   goodWillHunting,
