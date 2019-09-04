bc7a2b303e12e4424e7753dce2708cc75c13df05
Caleb Nance <calebnance@gmail.com>
2019-09-03 23:29:21 -0400

    fixed fonts for web build

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -1,10 +1,32 @@
 import { Platform } from 'react-native';
 
-const isiOS = Platform.OS === 'ios';
+const bold = Platform.select({
+  android: 'sans-serif-condensed',
+  ios: 'HelveticaNeue-Bold',
+  web: 'Helvetica Neue'
+});
+
+const light = Platform.select({
+  android: 'sans-serif-light',
+  ios: 'HelveticaNeue-Light',
+  web: 'Helvetica Neue'
+});
+
+const medium = Platform.select({
+  android: 'sans-serif-medium',
+  ios: 'HelveticaNeue-Medium',
+  web: 'Helvetica Neue'
+});
+
+const regular = Platform.select({
+  android: 'sans-serif',
+  ios: 'HelveticaNeue',
+  web: 'Helvetica Neue'
+});
 
 export default {
-  bold: isiOS ? 'HelveticaNeue-Bold' : 'sans-serif-condensed',
-  light: isiOS ? 'HelveticaNeue-Light' : 'sans-serif-light',
-  medium: isiOS ? 'HelveticaNeue-Medium' : 'sans-serif-medium',
-  regular: isiOS ? 'HelveticaNeue' : 'sans-serif'
+  bold,
+  light,
+  medium,
+  regular
 };
